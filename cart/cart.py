cc4f929cb3a8865317544cc6db4682b24aff47ec
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-26 10:18:08 +0200

    cart class and cart form

--- cart/cart.py
+++ cart/cart.py
@@ -13,11 +13,30 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
+    def __iter__(self):
         """
-        Add a product to the cart or update its quantity
+        Iterate over the items in the cart
+        and get the products from the database
+        """
+        product_ids = self.cart.keys()
+        # Get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
 
-        """
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        # Count all items in the cart
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def add(self, product, quantity=1, update_quantity=False):
+        # Add a product to the cart or update its quantity
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
@@ -31,3 +50,19 @@ class Cart(object):
     def save(self):
         # Mark the session as 'modified' to make sure it gets saved
         self.session.modified = True
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity']
+                   for item in self.cart.values())
+
+    def clear(self):
+        # Remove cart from sessions
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+        
