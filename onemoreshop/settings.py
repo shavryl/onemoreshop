0b07d60d4ae7742812fc168b56484f7987356add
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-26 09:42:58 +0200

    templates and static product list and details

--- onemoreshop/settings.py
+++ onemoreshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CART_SESSION_ID = 'cart'
+
 
 ROOT_URLCONF = 'onemoreshop.urls'
 
@@ -120,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
