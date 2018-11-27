ec5ed14e176e5f8a5904af4b569cfcdf0c902ac2
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-27 17:47:43 +0200

    collected static and send PDF invoice by email

--- onemoreshop/settings.py
+++ onemoreshop/settings.py
@@ -137,7 +137,11 @@ Configuration.configure(
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = 'http://127.0.0.1:8000/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_URL = '/media/'
 
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
 from orders.models import Order
+from io import BytesIO
+import weasyprint
 import braintree
 
 
@@ -20,6 +25,27 @@ def payment_process(request):
             # store unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # create invoice e-mail
+            subject = 'onemoreshop - Invoice no. {}'.format(order.id)
+            message = 'Please, find attached the invoice \
+                        for your recent purchase.'
+            email = EmailMessage(subject,
+                                 message,
+                                 'gavrilchenko.stanislav@gmail.com',
+                                 [order.email])
+            # generate PDF
+            html = render_to_string('orders/order/pdf.html',
+                                    {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(
+                            settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html)\
+                            .write_pdf(out, stylesheets=stylesheets)
+            # attach PDF file
+            email.attach('order_{}.pdf'.format(order.id),
+                         out.getvalue(), 'application/pdf')
+            email.send()
+
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
