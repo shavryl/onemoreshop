d42ee934561b059469c27b5df80be4ecc7a56b29
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-27 14:59:40 +0200

    admin custom order details view

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.http import HttpResponse
+from .models import Order, OrderItem
 import csv
 import datetime
+
+
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(
+        reverse('orders:admin_order_detail', args=[obj.id])))
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -40,7 +47,7 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
