83bf2179b0c90d16529e30dcee74be3f1db2418b
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-27 19:25:10 +0200

    template and view for discount coupon

--- cart/cart.py
+++ cart/cart.py
@@ -38,6 +38,12 @@ class Cart(object):
         # Count all items in the cart
         return sum(item['quantity'] for item in self.cart.values())
 
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
     def add(self, product, quantity=1, update_quantity=False):
         # Add a product to the cart or update its quantity
         product_id = str(product.id)
@@ -60,11 +66,30 @@ class Cart(object):
             del self.cart[product_id]
             self.save()
 
-    def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity']
-                   for item in self.cart.values())
-
     def clear(self):
         # Remove cart from sessions
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity']
+                   for item in self.cart.values())
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) \
+                * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
+
+
+
+
+
+
+
+
+
+
