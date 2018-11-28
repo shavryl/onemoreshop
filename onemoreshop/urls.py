b29f3b5eee332bd76472a6a80184d607190c1e6c
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-28 10:10:11 +0200

    tags for language switch, locale messages

--- onemoreshop/urls.py
+++ onemoreshop/urls.py
@@ -2,17 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('coupons/', include('coupons.urls', namespace='coupons')),
-    path('payment/', include('payment.urls', namespace='payment')),
-    path('orders/', include('orders.urls', namespace='orders')),
-    path('cart/', include('cart.urls', namespace='cart')),
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
+    path(_('coupons/'), include('coupons.urls', namespace='coupons')),
+    path(_('payment/'), include('payment.urls', namespace='payment')),
+    path(_('orders/'), include('orders.urls', namespace='orders')),
+    path(_('cart/'), include('cart.urls', namespace='cart')),
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
